3057ba5b4746900fe2c568e8df729e6446ad1c72
JoanMburu <joan.wangui@student.moringaschool.com>
2024-04-06 01:09:10 +0300

    Creates function labs

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+// Getting the first two drivers in an array
+const scuberDrivers = ['Antonia','Nuru','Amari','Mo'];
+    function returnFirstTwoDrivers(){
+       return scuberDrivers.slice(0,2);
+    }
+// Getting the last two drivers in an array
+function returnLastTwoDrivers (){
+    return scuberDrivers.slice(-2);
+}
+// Returns output of the functions returnFirstTwoDrivers & returnLastTwoDrivers
+const selectingDrivers =  [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+// Doubling & tripling fare
+function createFareMultiplier(multiplier){
+    return function (value){
+        return value * multiplier;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+// Selecting different drivers
+function selectDifferentDrivers (arrayOfDrivers,driverSelectorFunction){
+    return driverSelectorFunction(arrayOfDrivers);
+}
+
+
